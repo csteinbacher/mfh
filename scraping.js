14519730c4149968f861284813d76243d6a85ee6
Chris Steinbacher <steinbacherdesign@gmail.com>
2016-03-03 14:26:53 -0800

    unknown changes

--- scraping.js
+++ scraping.js
@@ -1,15 +1,43 @@
 var request = require('request'),
 	cheerio = require('cheerio'),
-	urls = [];
+	urls = [],
+	poundForPound = [];
+	allTheFighters = [];
 
-request('http://google.com/', function(err,resp,body){
+request('http://www.ufc.com/rankings', function(err,resp,body){
 	if ( !err && resp.statusCode == 200 ){
 		var $ = cheerio.load(body);
-		
-		console.log($('#hplogo').attr('src'));
-		//$('img').each(urls.push($(this).attr('href')));
-		//urls.push()
-		//console.log(urls)
+
+		var allFightersNames = $('td.name-column')
+        console.log(allFightersNames.length);
+
+        $(allFightersNames).each(function(){
+            var name = $(this).find('a').text();
+            name = name.trim();
+
+            var fighterPageUrl = $(this).find('a').attr('href');
+            fighterObj = [];
+            fighterObj.name = name;
+            fighterObj.fighterPageUrl = fighterPageUrl;
+        	allTheFighters.push(fighterObj);
+        })
+
+        //console.log(allTheFighters);
+
+        poundForPound = allTheFighters.slice(0,15);
+        console.log(poundForPound);
+
+   		//test write body to text file
+		// var datBody = $('body').html();
+
+		//fs = require('fs');
+		// fs.writeFile('bodytest.txt', flyWeightList, function (err) {
+		//   if (err) return console.log(err);
+		//   // console.log('Hello World > helloworld.txt');
+		// });
+
+
+
 
 	}
 })
