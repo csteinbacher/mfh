b31e2e6dc5d4caa666b5f093c1a1c4fc4b122ff1
Chris Steinbacher <steinbacherdesign@gmail.com>
2016-03-05 22:48:27 -0800

    dont worry about it

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var app = express();
+var request = require('request'),
+    cheerio = require('cheerio'),
+    mongoose = require('mongoose'),
+    express = require('express'),
+    app = express(),
+    poundForPound = [];
+    allTheFighters = [];
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,49 +22,94 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-//todo chris started here.
+//connect to mongodb and run init
+var db = mongoose.connect('mongodb://heroku_ghm8w9df:29up8jqas4q2re8nlh3irv8vk2@ds059938.mlab.com:59938/heroku_ghm8w9df',function(){
+    if ( mongoose.connection.readyState == 1 ){
+        console.log('connnected to mlbab db ('+mongoose.connection.readyState+') \n calling init');
+        init();
+    }
+});
 
-
-var request = require('request'),
-	cheerio = require('cheerio'),
-	urls = [],
-	poundForPound = [];
-	allTheFighters = [];
-
-request('http://www.ufc.com/rankings', function(err,resp,body){
-	if ( !err && resp.statusCode == 200 ){
-		var $ = cheerio.load(body);
-
-		var allFightersNames = $('td.name-column')
-        console.log(allFightersNames.length);
-
-        $(allFightersNames).each(function(){
-            var name = $(this).find('a').text();
-            name = name.trim();
-
-            var fighterPageUrl = $(this).find('a').attr('href');
-            fighterObj = [];
-            fighterObj.name = name;
-            fighterObj.fighterPageUrl = fighterPageUrl;
-        	allTheFighters.push(fighterObj);
-        })
-
-        //console.log(allTheFighters);
-
-        poundForPound = allTheFighters.slice(0,15);
-        console.log(poundForPound);
-
-   		//test write body to text file
-		// var datBody = $('body').html();
-
-		//fs = require('fs');
-		// fs.writeFile('bodytest.txt', flyWeightList, function (err) {
-		//   if (err) return console.log(err);
-		//   // console.log('Hello World > helloworld.txt');
-		// });
-
-
-
-
-	}
+//define different schemas 
+var fighterSchema = new db.Schema({
+    fighter_name : String,
+    fighter_page_uri : String
 })
+
+function testUsernamePop(){
+    // db = mongoose.connect('mongodb://heroku_ghm8w9df:29up8jqas4q2re8nlh3irv8vk2@ds059938.mlab.com:59938/heroku_ghm8w9df');
+
+    // var fighterObj = {fighter_name:'Fred',fighter_page_uri:'http://ufc.com/fighter/fred-the-fighter'};
+
+    // db.model('Fighters',fighterSchema).create(fighterObj, function(err,user){
+    //     console.log('e- ' + err)
+    //     console.log('u- ' + user)
+    // })   
+}
+
+function init(){
+    //pull the rankings 
+    pullRankingsList();
+}
+
+function pullRankingsList(){
+    request('http://www.ufc.com/rankings', function(err,resp,body){
+        if ( !err && resp.statusCode == 200 ){
+            var $ = cheerio.load(body);
+
+            var allFightersNames = $('td.name-column')
+            console.log(allFightersNames.length);
+
+            $(allFightersNames).each(function(){
+                var name = $(this).find('a').text();
+                name = name.trim();
+
+                var fighterPageUri = $(this).find('a').attr('href');
+                fighterObj = [];
+                fighterObj.name = name;
+                fighterObj.fighterPageUri = fighterPageUri;
+                allTheFighters.push(fighterObj);
+            })
+
+            //console.log(allTheFighters);
+
+            poundForPound = allTheFighters.slice(0,15);
+            updatePoundForPound(poundForPound);
+            // console.log(poundForPound);
+
+            //test write body to text file
+            // var datBody = $('body').html();
+
+            //fs = require('fs');
+            // fs.writeFile('bodytest.txt', flyWeightList, function (err) {
+            //   if (err) return console.log(err);
+            //   // console.log('Hello World > helloworld.txt');
+            // });
+
+        }
+    })
+}
+
+function updatePoundForPound(poundForPound){
+    //empty out the collection first
+    db.model('poundforpoundrankings',fighterSchema).remove({}, function(err,success){
+        console.log('error - ' + err)
+        console.log('success - removed all entries ' + success)
+
+        //populate the pound for pound
+        for ( i = 0; i < poundForPound.length; i++ ){
+            var fighterObj = {fighter_name:poundForPound[i].name,fighter_page_uri:poundForPound[i].fighterPageUri};
+
+            db.model('poundforpoundrankings',fighterSchema).create(fighterObj, function(err,user){
+                console.log('e- ' + err)
+                console.log('u- ' + user)
+            })
+
+        }
+    })
+    
+}
+
+
+
+console.log('err thangs oks');
